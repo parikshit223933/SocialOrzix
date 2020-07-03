fe419398ff73f50638b722ca3db3ae9325d418a8
Parikshit Singh <pk223933@gmail.com>
2020-07-03 19:57:56 +0530

    added store

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from '../reducers';
+import logger from 'redux-logger';
+
+export function configureStore() {
+    let store;
+    store = createStore(reducer, applyMiddleware(thunk, logger));
+    return store;
+}
