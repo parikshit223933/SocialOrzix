c2ef712f78caa0d6115fbfaed1e06f3cae7ab21f
Parikshit Singh <pk223933@gmail.com>
2020-07-04 01:10:47 +0530

    Add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import PropTypes from 'prop-types';
 
 import { fetchPosts } from "../actions/posts";
 import { PostsList } from './'
@@ -16,6 +17,59 @@ class App extends React.Component
         const { posts } = this.props;
         return (
             <div>
+                <nav className="nav">
+                    <div className="left-div">
+                        <img src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png" alt="logo" />
+                    </div>
+                    <div className="search-container">
+                        <img
+                            className='search-icon'
+                            src="https://image.flaticon.com/icons/svg/483/483356.svg"
+                            alt="search-icon" />
+                        <input placeholder="search" />
+                        <div className="search-results">
+                            <ul>
+                                <li className="search-results-row">
+                                    <img
+                                        src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                                        alt="user-dp" />
+                                    <span>
+                                        John Doe
+                                    </span>
+                                </li>
+                                <li className="search-results-row">
+                                    <img
+                                        src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                                        alt="user-dp" />
+                                    <span>
+                                        John Doe
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="right-nav">
+                        <div className="user">
+                            <img
+                                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                                alt="user-dp"
+                                id="user-dp" />
+                            <span>
+                                John Doe
+                            </span>
+                        </div>
+                        <div className="nav-links">
+                            <ul>
+                                <li>
+                                    Log In
+                                </li>
+                                <li>
+                                    Log Out
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
                 <PostsList posts={posts} />
             </div>
         );
@@ -28,4 +82,9 @@ function mapStateToProps(state)
         posts: state.posts
     };
 }
+
+App.propTypes = {
+    posts: PropTypes.array.isRequired
+}
+
 export default connect(mapStateToProps)(App);
--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PropTypes from 'prop-types';
 class PostsList extends React.Component
 {
     render()
@@ -72,6 +72,10 @@ class PostsList extends React.Component
             </div>
         );
     }
+}
+console.log(PostsList.propTypes);
+PostsList.propTypes = {
+    posts: PropTypes.array.isRequired
 }
 
 export default PostsList;
